e882b754f9aa46ab305bed75f1bce808c57037f2
giswqs <giswqs@users.noreply.github.com>
2026-03-24 15:51:37 +0000

    deploy: 082aee508cde42d7617d6cb00aeb055b4917cbb6

--- create_index.py
+++ create_index.py
@@ -44,7 +44,7 @@ def generate_index_html(directories, output_file):
 
 
 # Specify the directories to list
-directories_to_list = ["gispro", "duckdb"]
+directories_to_list = ["gispro", "duckdb", "geoai"]
 # Specify the output HTML file
 output_html_file = "index.html"
 
