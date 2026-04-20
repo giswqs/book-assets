c4fe48cdb3fd77f2b72335276ecde0099cfb3f0a
Qiusheng Wu <giswqs@gmail.com>
2026-04-20 09:02:02 -0400

    Add 'gee' and 'bundle' to directories to list

--- create_index.py
+++ create_index.py
@@ -44,7 +44,7 @@ def generate_index_html(directories, output_file):
 
 
 # Specify the directories to list
-directories_to_list = ["gispro", "duckdb", "geoai"]
+directories_to_list = ["gispro", "duckdb", "geoai", "gee", "bundle"]
 # Specify the output HTML file
 output_html_file = "index.html"
 
